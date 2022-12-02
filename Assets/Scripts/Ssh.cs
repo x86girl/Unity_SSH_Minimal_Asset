bec2cb6de9df27a52cfd8fbc28a991ad77539526
Priscila Gutierres <priscila.gutierres@gmail.com>
2022-12-02 20:36:24 -0300

    Initial Commit

---
+++ Assets/Scripts/Ssh.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Renci.SshNet;
+
+public class Ssh 
+{
+// Create an SSH object to send various kinds of data!
+}
