38f893d5389fadb01a50af99f9982fb423cc5378
Priscila Gutierres <priscila.gutierres@gmail.com>
2022-12-03 12:45:40 -0300

    Create a class inside ./Assets/Scripts/Ssh.cs Create a method for password connection

--- Assets/Scripts/Ssh.cs
+++ Assets/Scripts/Ssh.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Renci.SshNet;
+using System.IO;
 
-public class Ssh 
+
+public class Ssh
 {
+    private string Host;
+    private string UserName;
+
+    
+   void Create_password_connection(string Host, string UserName, string Password)
+    {
+        ConnectionInfo ConnNfo = new ConnectionInfo(Host, 22, UserName,
+new AuthenticationMethod[]{
+       new PasswordAuthenticationMethod(UserName,Password),
+
+});
+    }
+
+
 // Create an SSH object to send various kinds of data!
 }
