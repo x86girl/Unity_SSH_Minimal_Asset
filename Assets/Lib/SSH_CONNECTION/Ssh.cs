768b0d7f9a586873fd64f33ea04506f3d3a9c350
Priscila Gutierres <priscila.gutierres@gmail.com>
2022-12-04 09:05:41 -0300

    Create a separete working class for SSH, with a constructor

---
+++ Assets/Lib/SSH_CONNECTION/Ssh.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Renci.SshNet;
+using System.IO;
+
+namespace SshConnect
+{
+    public class Ssh
+    {
+
+        string Host;
+        string UserName;
+
+    public Ssh(string Host_, string UserName_)
+        {
+            this.Host = Host_;
+            this.UserName = UserName_;
+        }
+
+        void Create_password_connection(string Host, string UserName, string Password)
+        {
+            ConnectionInfo ConnNfo = new ConnectionInfo(Host, 22, UserName,
+    new AuthenticationMethod[]{
+       new PasswordAuthenticationMethod(UserName,Password),
+
+    });
+        }
+
+
+        // Create an SSH object to send various kinds of data!
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  SshConnect;
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var ssh_instance = new SshConnect.Ssh("localhost", "priscila");
+        Debug.Log("It is working!");
+        ssh_instance.hello();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
